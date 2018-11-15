42e6763c0319b8b1224558f2b9ff0da1aa502c3a
Blues <40874591+ABJervis@users.noreply.github.com>
2018-11-15 13:38:24 -0500

    beginning

---
+++ app/data/friends.js
@@ -0,0 +1,90 @@
+creaturesArray = [
+
+    {
+        "name":"Pretty Kitty",
+        "photo":"",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+
+      {
+        "name":"Fluffy Doggy",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }, 
+
+      {
+        "name":"Wicked Chameleon",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }, 
+
+      {
+        "name":"Rockin Rabbit",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }, 
+
+      {
+        "name":"Wise Owl",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }, 
+
+]
+
+module.exports = creaturesArray;
