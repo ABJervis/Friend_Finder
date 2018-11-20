05e7f9968e58dd7b7f39228696adebb83ccf9cea
Blues <40874591+ABJervis@users.noreply.github.com>
2018-11-19 22:27:14 -0500

    work on logic and routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ creaturesArray = [
 
     {
         "name":"Pretty Kitty",
-        "photo":"",
+        "photo":"https://welovecatsandkittens.com/wp-content/uploads/2013/10/pretty-cat.jpg",
         "scores":[
             5,
             1,
@@ -15,74 +15,79 @@ creaturesArray = [
             4,
             1
           ]
+          //32
       },
 
       {
         "name":"Fluffy Doggy",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi2keq0ktzeAhURTawKHStBDvkQjRx6BAgBEAU&url=http%3A%2F%2Fattackofthecute.com%2Fon%2F%3Fi%3D24451&psig=AOvVaw1Qw5MAM6TxYJfmcN5jhm_Y&ust=1542568686411443",
         "scores":[
-            5,
-            1,
-            4,
+            3,
+            2,
             4,
             5,
             1,
             2,
+            4,
             5,
             4,
             1
           ]
+          //31
       }, 
 
       {
         "name":"Wicked Chameleon",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F5f%2F30%2F4e%2F5f304ee6df8bf665f64aa9ae2430f8bf.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.ch%2Fpin%2F340162578074515462%2F&docid=UlBQGFRwWV0OZM&tbnid=hc-k1ZIb6ApaCM%3A&vet=10ahUKEwisntzPktzeAhWK6oMKHYsvA0gQMwhBKAIwAg..i&w=500&h=415&itg=1&bih=695&biw=1517&q=wicked%20chameleon&ved=0ahUKEwisntzPktzeAhWK6oMKHYsvA0gQMwhBKAIwAg&iact=mrc&uact=8",
         "scores":[
             5,
             1,
-            4,
-            4,
-            5,
-            1,
             2,
+            4,
+            3,
+            1,
             5,
+            3,
             4,
             1
           ]
+          //sum 29
       }, 
 
       {
         "name":"Rockin Rabbit",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.petguide.com%2Fwp-content%2Fuploads%2F2016%2F05%2Fflorida-white.jpg&imgrefurl=https%3A%2F%2Fwww.petguide.com%2Fbreeds%2Frabbit%2Fflorida-white-rabbit%2F&docid=aQJ8CzKSJZ0DBM&tbnid=PJxgwkGvQDlcyM%3A&vet=10ahUKEwig2_fpktzeAhWLm4MKHUtFA74QMwiMASgVMBU..i&w=637&h=421&bih=695&biw=1517&q=rabbit&ved=0ahUKEwig2_fpktzeAhWLm4MKHUtFA74QMwiMASgVMBU&iact=mrc&uact=8",
         "scores":[
-            5,
+            4,
             1,
+            3,
             4,
-            4,
-            5,
+            2,
             1,
             2,
-            5,
             4,
-            1
+            1,
+            5
           ]
+          //sum 27
       }, 
 
       {
         "name":"Wise Owl",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://www.google.com/imgres?imgurl=http%3A%2F%2Fs3.amazonaws.com%2Fogden_images%2Fwww.timesrepublican.com%2Fimages%2F2017%2F12%2F14102409%2FSnowy-owl-620x840.jpg&imgrefurl=http%3A%2F%2Fwww.timesrepublican.com%2Fnews%2F2017%2F12%2Fsnowy-owl-seen-near-clemons%2F&docid=VdWr46abF9hOdM&tbnid=BydKxa3HpitHDM%3A&vet=10ahUKEwj-oa2ok9zeAhWhmeAKHZaMDFwQMwiRASgaMBo..i&w=620&h=840&bih=695&biw=1517&q=snowy%20owl&ved=0ahUKEwj-oa2ok9zeAhWhmeAKHZaMDFwQMwiRASgaMBo&iact=mrc&uact=8",
         "scores":[
+            3,
             5,
-            1,
             4,
             4,
             5,
-            1,
+            3,
             2,
-            5,
+            1,
             4,
-            1
+            3
           ]
+          //sum 34
       }, 
 
 ]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,3 +3,40 @@
 A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 A POST routes /api/friends. This will be used to handle incoming survey results. 
 This route will also be used to handle the compatibility logic.*/
+
+//what variables are needed? Creatures Array & survey results
+
+var creaturesArray = require("../data/friends");
+var userAnswers = require("../public/survey");
+
+//routing
+
+module.exports = function(app) {
+
+    app.get("/api/creatures", function(req, res) {
+        res.json(creaturesArray);
+      });
+    
+      app.get("/api/survey", function(req, res) {
+        res.json(userAnswers);
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+}
